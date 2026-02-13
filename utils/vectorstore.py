94183069104111986654711257a1e3b3207c8af0
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2026-02-13 11:29:22 +0530

    Update vectorstore.py

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,1 +1,37 @@
+import os
+import faiss
+import pickle
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def create_vectorstore(texts):
+
+    embeddings = model.encode(texts)
+
+    dimension = embeddings.shape[1]
+
+    index = faiss.IndexFlatL2(dimension)
+
+    index.add(embeddings)
+
+    with open("vectorstore/texts.pkl", "wb") as f:
+        pickle.dump(texts, f)
+
+    faiss.write_index(index, "vectorstore/index.faiss")
+
+def retrieve(query, k=3):
+
+    index = faiss.read_index("vectorstore/index.faiss")
+
+    with open("vectorstore/texts.pkl", "rb") as f:
+        texts = pickle.load(f)
+
+    query_embedding = model.encode([query])
+
+    distances, indices = index.search(query_embedding, k)
+
+    results = [texts[i] for i in indices[0]]
+
+    return results
 
